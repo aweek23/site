84b6d2a519731bc5c54c569768444b3f9d54e682
Ayzirix <ayzirix@gmail.com>
2022-02-06 19:51:09 +0100

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,34 @@
+const config = {
+    USERNAME: "αωєєк#8041",
+    PICTURE: "assets/img/aweek.png",
+
+    DESCRIPTION: "Salut ! Je suis Ayzirix, mieux connu sous le pseudonyme de Aweek. J'ai 16ans et je suis belge, je dis alors nonante et non quatre-vingt dix. Je suis développeur débutant, hébergeur et technicien, infographiste, community manager et fort présent dans le monde des cryptomonnaies. N'hésitez pas à passer sur ma page Github afin d'avoir plus d'information sur ma personnes, mes compétences et/ou mes projets.",
+
+    BADGES: [
+        { id: "HypeSquad Brillance", image: "assets/img/brillance.png" },
+        { id: "Png", image: "assets/img/png.png" },
+        { id: "Dev", image: "assets/img/dev.png" },
+        { id: "Hosting", image: "assets/img/heberg.png" },
+        { id: "Crypto", image: "assets/img/btc.png" },
+        { id: "Infographisme", image: "assets/img/infographisme.png" },
+        { id: "Community-Management", image: "assets/img/cm.png" },
+        { id: "Png", image: "assets/img/png.png" },
+        { id: "Html", image: "assets/img/html.png" },
+        { id: "Css", image: "assets/img/css.png" },
+        { id: "Js", image: "assets/img/js.png" },
+        { id: "Php", image: "assets/img/php.png" },
+    ],
+
+    PROJECTS: [
+        { id: "αωєєк.bot", image: "assets/img/aweek.png", link: "", name: "αωєєк.bot - Développeur principal" },
+        { id: "genius", image: "assets/img/genius.png", link: "https://discord.gg/97YCtxmDF4", name: "Genius, astuces et cours privés - Owner" },
+        { id: "anarchy", image: "assets/img/anarchy.png", link: "https://discord.gg/45dwFAGJpT", name: "Anarchy-Mc - Fondateur" },
+        { id: "faucetCrypto", image: "assets/img/png.png", link: "https://faucet-crypto.tk", name: "FaucetCrypto - Développeur/Owner" },
+        { id: "hostyourwebsite", image: "assets/img/png.png", link: "", name: "HostYourWebsite - Administration" },
+        { id: "pi-heberg", image: "assets/img/pi-heberg.png", link: "https://pi-heberg.fr", name: "Pi-Heberg - Community-Manager" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/Ayzirix" },
+    ],
+}
