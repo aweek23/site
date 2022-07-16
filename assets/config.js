0715c2303eb2107eeafb8eb2376e430ed20b90a5
Aweek <aweek.mail@gmail.com>
2022-07-16 11:18:21 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -9,10 +9,13 @@ const config = {
     ],
 
     PROJECTS: [
-        { id: "Legion", image: "assets/img/A2219EC7-521F-41E5-AA93-1F81A080D8B6.png", link: "https://discord.gg/A8sNE6vbG9", name: "Legion, formations et cours privés - Owner" },
-        { id: "SocialBoost", image: "assets/img/66E3CBA2-8396-4575-9043-6C45753420ED.png", link: "https://discord.gg/VDnPdsH2m3", name: "SocialBoost, boost tes réseaux - Owner" },
-        { id: "ShopDeals", image: "assets/img/66E3CBA2-8396-4575-9043-6C45753420ED.png", link: "https://discord.gg/h9xkQrj4vS", name: "ShopDeals, achete tout moins cher - Owner" },
-        { id: "SurvieMC", image: "assets/img/AC1D265B-8B8B-4B59-ADE8-9D701B22FAE8.png", link: "https://discord.gg/KpgYbP2GUE", name: "Survie Minecraft moddée multijoueurs crossplateformes- Administrateur" },
+        { id: "Astra Giveaways", image: "", link: "", name: "" },
+        { id: "Social Boost", image: "", link: "", name: "" },
+        { id: "ShopDeals", image: "", link: "", name: "" },
+        { id: "Legion", image: "", link: "", name: "" },
+        { id: "Casino Palace", image: "", link: "", name: "" },
+        { id: "Aweek.bot", image: "", link: "", name: "" },
+        { id: "S3MC", image: "", link: "", name: "" },
     ],
 
     SOCIALS: [
